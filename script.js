e0e059d2d8aa648857a0ac8e25417caa28a2c864
JOAO VICTOR LOEWEN <joao.loewen@alunos.sc.senac.br>
2025-03-21 14:37:37 -0300

    Add botão trocar sinal

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 function adicionar(valor) {
     document.getElementById("display").value += valor;
 }
+
 function limpar() {
     document.getElementById("display").value = "";
 }
+
 function calcular() {
     try {
         document.getElementById("display").value = eval(document.getElementById("display").value);
     } catch {
         document.getElementById("display").value = "Erro";
+    }
+}
+
+function trocarSinal() {
+    let display = document.getElementById("display");
+    let valor = display.value;
+
+    if (valor !== "") {
+        if (valor.startsWith("-")) {
+            display.value = valor.substring(1);
+        } else {
+            display.value = "-" + valor;
+        }
     }
 }
